dca2b8674705e7d8e0a36e6e9d524cf8a0f87a1f
Vishal Yadav <yadavvi91@gmail.com>
2014-12-25 17:31:18 +0530

    Moved classes to new files

---
+++ src/com/yadavvi/stringsearch/StringSearch.java
@@ -0,0 +1,29 @@
+package com.yadavvi.stringsearch;
+
+public class StringSearch {
+
+	public static void main(String[] args) {
+		/*String text = "abacadabrabracabracadabrabrabracad";
+		String pattern = "abra";*/
+		/*String text = "AAAAAAAAAB";
+		String pattern = "AAAAB";*/
+		String text = "abacadabrabracabracadabrabrabracad";
+		String pattern = "abracadabra";
+
+		BruteForce bf = new BruteForce();
+		int value;
+		value = bf.search(pattern, text);
+
+		System.out.println("The pattern is found at: " + value);
+
+		value = bf.searchOther(pattern, text);
+		System.out.println("The Other pattern is found at: " + value);
+
+		KMP kmp = new KMP(pattern.toCharArray());
+		value = kmp.search(text);
+		System.out.println("The KMP pattern is found at: " + value);
+
+		value = kmp.searchOther(text);
+		System.out.println("The Other KMP pattern is found at: " + value);
+	}
+}
